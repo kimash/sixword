8ea2970af997b00cba35f6b888b75060143c41dd
Kim Ash <ash.kimberly.b@gmail.com>
2012-10-17 23:17:39 -0400

    removed email

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,7 +2,7 @@
 $("#addCaptionbtn").click(function() {
  caption=$("#punc1").val()+$("#input1").val()+$("#punc2").val()+$("#input2").val()+$("#punc3").val()+$("#input3").val()+$("#punc4").val()+$("#input4").val()+$("#punc5").val()+$("#input5").val()+$("#punc6").val()+$("#input6").val()+$("#punc7").val();
   console.log(caption);
-  $.post("/addcaption", { 'text':caption, 'name' : $("#nameInput").val(), 'email': $("#emailInput").val() },function(data) {
+  $.post("/addcaption", { 'text':caption, 'name' : $("#nameInput").val()},function(data) {
      location.reload(); 
    });
 });
